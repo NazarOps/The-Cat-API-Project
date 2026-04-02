b4b0c20504f0bc690ab545e98a5434276a579a56
Nazar Kokovkin <lilnz4@hotmail.com>
2026-04-02 20:14:48 +0200

    automapper implemented in getallcatsasync() method, getcatbyidasync() and createcatasync()

---
+++ Profiles/CatProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Cat_API_Project.Models;
+using Cat_API_Project.DTO;
+
+namespace Cat_API_Project.Profiles
+{
+    public class CatProfile : Profile // inheret from automapper profile
+    {
+        public CatProfile()
+        {
+            CreateMap<CreateCatDTO, Cat>();
+
+            CreateMap<Cat, CatDTO>()    //map from cat to catDTO automatically
+                .ForMember(dest => dest.BreedName,
+                opt => opt.MapFrom(src => src.Breed.BreedName));
+
+            CreateMap<Breed, BreedDTO>();
+        }
+    }
+}
--- Services/CatService.cs
+++ Services/CatService.cs
@@ -4,31 +4,41 @@ using Cat_API_Project.Models;
 using Cat_API_Project.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography.X509Certificates;
+using AutoMapper;
 
 namespace Cat_API_Project.Services
 {
     public class CatService : ICatService
     {
         private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
 
-        public CatService(AppDbContext context)
+        public CatService(AppDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
+
         }
 
         public async Task<List<CatDTO>> GetAllCatsAsync()
         {
-            return await _context.Cats
-                .Include(c => c.Breed) // include breed that the cat belongs to
-                .Select(c => new CatDTO
-                {
-                    Id = c.Id,
-                    Name = c.Name,
-                    Description = c.Description,
-                    BreedId = c.BreedId,
-                    BreedName = c.Breed.BreedName // if we didn't include breed then this would be null
-                })
-                .ToListAsync();  
+            //return await _context.Cats
+            //    .Include(c => c.Breed) // include breed that the cat belongs to
+            //    .Select(c => new CatDTO
+            //    {
+            //        Id = c.Id,
+            //        Name = c.Name,
+            //        Description = c.Description,
+            //        BreedId = c.BreedId,
+            //        BreedName = c.Breed.BreedName // if we didn't include breed then this would be null
+            //    })
+            //    .ToListAsync();  
+
+            var cats = await _context.Cats
+                .Include(c => c.Breed)
+                .ToListAsync(); //get all cats first then use automapper
+
+            return _mapper.Map<List<CatDTO>>(cats);
         }
 
         public async Task<CatDTO?> GetCatByIdAsync(int id) // get one cat, if it does not exist return null 404
@@ -42,14 +52,16 @@ namespace Cat_API_Project.Services
                 return null;
             }
 
-            return new CatDTO
-            {
-                Id = cat.Id,
-                Name = cat.Name,
-                Description = cat.Description,
-                BreedId = cat.BreedId,
-                BreedName = cat.Breed.BreedName
-            };
+            //return new CatDTO
+            //{
+            //    Id = cat.Id,
+            //    Name = cat.Name,
+            //    Description = cat.Description,
+            //    BreedId = cat.BreedId,
+            //    BreedName = cat.Breed.BreedName
+            //};
+
+            return _mapper.Map<CatDTO>(cat);
         }
 
         public async Task<CatDTO?> CreateCatAsync(CreateCatDTO createCatDTO)
@@ -62,12 +74,14 @@ namespace Cat_API_Project.Services
                 return null;
             }
 
-            var cat = new Cat // if breed exists, initialize an object
-            {
-                Name = createCatDTO.Name,
-                Description = createCatDTO.Description,
-                BreedId = createCatDTO.BreedId
-            };
+            //var cat = new Cat // if breed exists, initialize an object
+            //{
+            //    Name = createCatDTO.Name,
+            //    Description = createCatDTO.Description,
+            //    BreedId = createCatDTO.BreedId
+            //};
+
+            var cat = _mapper.Map<Cat>(createCatDTO); // use automapper instead of manually assigning object above
 
             await _context.Cats.AddAsync(cat); // save object
             await _context.SaveChangesAsync();
@@ -81,14 +95,16 @@ namespace Cat_API_Project.Services
                 return null;
             }
 
-            return new CatDTO
-            {
-                Id = createdCat.Id,
-                Name = createdCat.Name,
-                Description = createdCat.Description,
-                BreedId = createdCat.BreedId,
-                BreedName = createdCat.Breed.BreedName
-            };
+            //return new CatDTO
+            //{
+            //    Id = createdCat.Id,
+            //    Name = createdCat.Name,
+            //    Description = createdCat.Description,
+            //    BreedId = createdCat.BreedId,
+            //    BreedName = createdCat.Breed.BreedName
+            //};
+
+            return _mapper.Map<CatDTO>(createdCat);
         }
 
         public async Task<bool> UpdateCatAsync(int id, UpdateCatDTO updateCatDTO) // checks if cat and breed exists, if it does then update all the fields
